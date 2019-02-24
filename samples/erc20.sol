ac121442719cdb1db2e81360cb3836f99a6c7545
Ether Authority <43878603+EtherAuthority@users.noreply.github.com>
2019-02-24 15:13:08 +0530

    Create erc20.sol

---
+++ samples/erc20.sol
@@ -0,0 +1,287 @@
+pragma solidity 0.5.4; /*
+
+___________________________________________________________________
+  _      _                                        ______           
+  |  |  /          /                                /              
+--|-/|-/-----__---/----__----__---_--_----__-------/-------__------
+  |/ |/    /___) /   /   ' /   ) / /  ) /___)     /      /   )     
+__/__|____(___ _/___(___ _(___/_/_/__/_(___ _____/______(___/__o_o_
+
+                                                                             
+  
+// ----------------------------------------------------------------------------
+// 'EtherAuthority' Token contract with following features
+//      => ERC20 Compliance
+//      => Higher degree of control by owner - safeguard functionality
+//      => SafeMath implementation 
+//      => Burnable and minting 
+//
+// Name        : EtherAuthority
+// Symbol      : EAT
+// Total supply: 0 
+// Decimals    : 18
+//
+// Contract designed by EtherAuthority ( https://EtherAuthority.io )
+// ----------------------------------------------------------------------------
+*/ 
+
+//*******************************************************************//
+//------------------------ SafeMath Library -------------------------//
+//*******************************************************************//
+    /**
+     * @title SafeMath
+     * @dev Math operations with safety checks that throw on error
+     */
+    library SafeMath {
+      function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+          return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+      }
+    
+      function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+      }
+    
+      function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+      }
+    
+      function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+      }
+    }
+
+
+//*******************************************************************//
+//------------------ Contract to Manage Ownership -------------------//
+//*******************************************************************//
+    
+    contract owned {
+        address payable public owner;
+        
+         constructor () public {
+            owner = msg.sender;
+        }
+    
+        modifier onlyOwner {
+            require(msg.sender == owner);
+            _;
+        }
+    
+        function transferOwnership(address payable newOwner) onlyOwner public {
+            owner = newOwner;
+        }
+    }
+    
+
+    
+//****************************************************************************//
+//---------------------        MAIN CODE STARTS HERE     ---------------------//
+//****************************************************************************//
+    
+contract EatherAuthorityToken is owned {
+    
+
+    /*===============================
+    =         DATA STORAGE          =
+    ===============================*/
+
+    // Public variables of the token
+    using SafeMath for uint256;
+    string public name = "EatherAuhority";
+    string public symbol = "EAT";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+    bool public safeguard = false;  //putting safeguard on will halt all non-owner functions
+
+    // This creates a mapping with all data storage
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+    mapping (address => bool) public frozenAccount;
+
+
+    /*===============================
+    =         PUBLIC EVENTS         =
+    ===============================*/
+
+    // This generates a public event of token transfer
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+        
+    // This generates a public event for frozen (blacklisting) accounts
+    event FrozenFunds(address target, bool frozen);
+
+
+
+    /*======================================
+    =       STANDARD ERC20 FUNCTIONS       =
+    ======================================*/
+
+    /* Internal transfer, only can be called by this contract */
+    function _transfer(address _from, address _to, uint _value) internal {
+        
+        //checking conditions
+        require(!safeguard);
+        require (_to != address(0x0));                      // Prevent transfer to 0x0 address. Use burn() instead
+        require(!frozenAccount[_from]);                     // Check if sender is frozen
+        require(!frozenAccount[_to]);                       // Check if recipient is frozen
+        
+        // overflow and undeflow checked by SafeMath Library
+        balanceOf[_from] = balanceOf[_from].sub(_value);    // Subtract from the sender
+        balanceOf[_to] = balanceOf[_to].add(_value);        // Add the same to the recipient
+        
+        // emit Transfer event
+        emit Transfer(_from, _to, _value);
+    }
+
+    /**
+        * Transfer tokens
+        *
+        * Send `_value` tokens to `_to` from your account
+        *
+        * @param _to The address of the recipient
+        * @param _value the amount to send
+        */
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        //no need to check for input validations, as that is ruled by SafeMath
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    /**
+        * Transfer tokens from other address
+        *
+        * Send `_value` tokens to `_to` in behalf of `_from`
+        *
+        * @param _from The address of the sender
+        * @param _to The address of the recipient
+        * @param _value the amount to send
+        */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    /**
+        * Set allowance for other address
+        *
+        * Allows `_spender` to spend no more than `_value` tokens in your behalf
+        *
+        * @param _spender The address authorized to spend
+        * @param _value the max amount they can spend
+        */
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(!safeguard);
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+
+    /*=====================================
+    =       CUSTOM PUBLIC FUNCTIONS       =
+    ======================================*/
+
+    /**
+        * Destroy tokens
+        *
+        * Remove `_value` tokens from the system irreversibly
+        *
+        * @param _value the amount of money to burn
+        */
+    function burn(uint256 _value) public returns (bool success) {
+        require(!safeguard);
+        //checking of enough token balance is done by SafeMath
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);  // Subtract from the sender
+        totalSupply = totalSupply.sub(_value);                      // Updates totalSupply
+        emit Burn(msg.sender, _value);
+        return true;
+    }
+
+    /**
+        * Destroy tokens from other account
+        *
+        * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+        *
+        * @param _from the address of the sender
+        * @param _value the amount of money to burn
+        */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(!safeguard);
+        //checking of allowance and token value is done by SafeMath
+        balanceOf[_from] = balanceOf[_from].sub(_value);                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value); // Subtract from the sender's allowance
+        totalSupply = totalSupply.sub(_value);                                   // Update totalSupply
+        emit  Burn(_from, _value);
+        return true;
+    }
+        
+    
+    /** 
+        * @notice `freeze? Prevent | Allow` `target` from sending & receiving tokens
+        * @param target Address to be frozen
+        * @param freeze either to freeze it or not
+        */
+    function freezeAccount(address target, bool freeze) onlyOwner public {
+            frozenAccount[target] = freeze;
+        emit  FrozenFunds(target, freeze);
+    }
+    
+    /** 
+        * @notice Create `mintedAmount` tokens and send it to `target`
+        * @param target Address to receive the tokens
+        * @param mintedAmount the amount of tokens it will receive
+        */
+    function mintToken(address target, uint256 mintedAmount) onlyOwner public {
+        balanceOf[target] = balanceOf[target].add(mintedAmount);
+        totalSupply = totalSupply.add(mintedAmount);
+        emit Transfer(address(0), target, mintedAmount);
+    }
+
+        
+
+    /**
+        * Owner can transfer tokens from tonctract to owner address
+        *
+        * When safeguard is true, then all the non-owner functions will stop working.
+        * When safeguard is false, then all the functions will resume working back again!
+        */
+    
+    function manualWithdrawTokens(uint256 tokenAmount) public onlyOwner{
+        // no need for overflow checking as that will be done in transfer function
+        _transfer(address(this), owner, tokenAmount);
+    }
+    
+
+    
+    /**
+        * Change safeguard status on or off
+        *
+        * When safeguard is true, then all the non-owner functions will stop working.
+        * When safeguard is false, then all the functions will resume working back again!
+        */
+    function changeSafeguardStatus() onlyOwner public{
+        if (safeguard == false){
+            safeguard = true;
+        }
+        else{
+            safeguard = false;    
+        }
+    }
+    
+
+
+}
